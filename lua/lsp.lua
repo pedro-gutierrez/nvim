11987623ece9a1993921ddb46947b97fabc930ab
Pedro Gutiérrez <pedrogutierrez@mac.com>
2022-10-28 01:28:00 +0200

    Show diagnostics in quickfix list

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,13 +4,25 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+local h = 'textDocument/publishDiagnostics'
+vim.lsp.handlers[h] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
-  virtual_text = true,
+  virtual_text = false,
   signs = true,
   update_in_insert = true
 })
+
+function Quickfixlist()
+  local current = vim.api.nvim_get_current_win()
+  vim.diagnostic.setqflist()
+  vim.cmd('cwindow')
+  if next(vim.diagnostic.get()) then
+    vim.fn.win_gotoid(current)
+  end
+end
+
+vim.cmd [[autocmd DiagnosticChanged * :lua Quickfixlist()]]
 
 require("mason").setup()
 require("mason-lspconfig").setup({
