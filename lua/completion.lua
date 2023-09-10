a79397f5d06108101fab7ce80e0ba5370e8a7382
Pedro Gutiérrez <pedrogutierrez@mac.com>
2023-09-10 19:06:42 +0200

    Completion tweaks

--- lua/completion.lua
+++ lua/completion.lua
@@ -15,8 +15,9 @@ cmp.setup({
   }),
   sources = {
     { name = 'path' },
-    { name = 'nvim_lsp',   keyword_length = 3 },
+    { name = 'nvim_lsp',   keyword_length = 1 },
     { name = 'buffer',     keyword_length = 3 },
+    { name = 'luasnip',    keyword_length = 2 },
     { name = 'treesitter', keyword_length = 3 }
   }
 })
