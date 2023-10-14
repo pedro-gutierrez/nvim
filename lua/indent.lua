37f8ab0371f99a1e8ba833ce649c73b428e6214c
Pedro Gutiérrez <pedrogutierrez@mac.com>
2023-10-14 07:37:00 +0200

    Refresh indentation configuration

--- lua/indent.lua
+++ lua/indent.lua
@@ -1,9 +1,15 @@
-require("indent_blankline").setup {
-  buftype_exclude = { "terminal", "startify" },
-  filetype_exclude = { 'help', 'packer', 'startify', 'alpha' },
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-  },
-}
-
 vim.cmd [[highlight IndentBlanklineIndent1 ctermfg=lightgray cterm=none]]
+
+require("ibl").setup {
+  exclude = {
+    filetypes = { "startify" },
+    buftypes = { "terminal" }
+  },
+  indent = {
+    char = "|",
+    highlight = {
+      "IndentBlanklineIndent1",
+    }
+  },
+  scope = { enabled = false }
+}
