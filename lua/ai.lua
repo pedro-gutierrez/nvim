3690153e207cf370385afbfb74868a3dd92e8ca2
Pedro Gutiérrez <pedrogutierrez@mac.com>
2024-10-11 11:27:08 +0200

    Fix avante setup

--- lua/ai.lua
+++ lua/ai.lua
@@ -1,8 +1,7 @@
---require("gp").setup {
---}
---
---
---require('img-clip').setup {}
---require('copilot').setup {}
---require('avante_lib').load()
---require('avante').setup {}
+require("gp").setup {
+}
+
+
+require('img-clip').setup {}
+require('copilot').setup {}
+require('avante_lib').load()
